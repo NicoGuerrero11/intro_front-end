1bd61bc9c6c8367b13c24f2afb4d66a3705d16ef
NicolasGuerrero <nicoguerrero0800@gmail.com>
2025-03-18 18:52:20 -0600

    intro-front

---
+++ bundlers/parcel/app.js
@@ -0,0 +1,8 @@
+import { cart, addToCart } from './shoppingCart.js';
+
+console.log(cart);
+console.log(addToCart);
+
+
+addToCart('shampoos', 10);
+console.log(cart);
---
+++ bundlers/parcel/dist/parcel.8216fdd0.js
@@ -0,0 +1,2 @@
+let o=[];function l(l,s){o.push({producto:l,cantidad:s}),console.log(`has agregado ${s} de ${l}`)}console.log(o),console.log(l),l("shampoos",10),console.log(o);
+//# sourceMappingURL=parcel.8216fdd0.js.map
